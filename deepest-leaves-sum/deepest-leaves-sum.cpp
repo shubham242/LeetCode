62f13fe5e9073f8978735a6144d1d14b2fd46b6c
shubham242 <58747139+shubham242@users.noreply.github.com>
2021-04-14 16:34:12 +0530

    Time: 44 ms (29.71%), Space: 38.2 MB (64.39%) - LeetHub

---
+++ deepest-leaves-sum/deepest-leaves-sum.cpp
@@ -0,0 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int lvl(TreeNode* root,int l){
+        if(root->left!=NULL&&root->right!=NULL)
+            return max(lvl(root->left,l+1),lvl(root->right,l+1));
+        else if(root->left!=NULL)
+            return lvl(root->left,l+1);
+        else if(root->right!=NULL)
+            return lvl(root->right,l+1);
+        else
+            return l; 
+    }
+    
+    int deepSum(TreeNode* root,int l,int sum,int deep){
+        if(l==deep)
+            sum+=root->val;
+        if(root->left!=NULL)
+            sum+=deepSum(root->left,l+1,0,deep); 
+        if(root->right!=NULL)
+            sum+=deepSum(root->right,l+1,0,deep);
+        return sum;
+    }
+        
+    int deepestLeavesSum(TreeNode* root) {
+        
+        int deep = lvl(root,1);
+        return deepSum(root,1,0,deep);
+        
+    }
+};
