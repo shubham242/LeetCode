1fe130e932b482058b5b8ad48a4ae686c146851b
Shubham <58747139+shubham242@users.noreply.github.com>
2021-07-24 23:06:57 +0530

    Time: 4 ms (75.15%), Space: 9.2 MB (43.75%) - LeetHub

--- longest-common-prefix/longest-common-prefix.cpp
+++ longest-common-prefix/longest-common-prefix.cpp
@@ -1,18 +1,39 @@
-class Solution {
+bool sortcol(const string &v1, const string &v2)
+{
+    return v1.size() < v2.size();
+}
+
+class Solution
+{
 public:
-    string com(string &s1,string &s2){
+    string com(string &s1, string &s2)
+    {
         int i;
-        for(i=0;i<min(s1.size(),s2.size());i++){
-            if(s1[i]!=s2[i])
+        for (i = 0; i < min(s1.size(), s2.size()); i++)
+        {
+            if (s1[i] != s2[i])
                 break;
         }
-        return s1.substr(0,i);
+        return s1.substr(0, i);
     }
-    string longestCommonPrefix(vector<string>& strs) {
-        
-        string ans=strs[0];
-        for(int i=1;i<strs.size();i++)
-            ans=com(ans,strs[i]);
-        return ans;
+    string longestCommonPrefix(vector<string> &strs)
+    {
+        sort(strs.begin(), strs.end(), sortcol);
+        int i;
+        for (i = 0; i < strs[0].size(); i++)
+        {
+            int f = 0;
+            for (int j = 1; j < strs.size(); j++)
+            {
+                if (strs[j][i] != strs[0][i])
+                {
+                    f = 1;
+                    break;
+                }
+            }
+            if (f)
+                break;
+        }
+        return strs[0].substr(0, i);
     }
 };
