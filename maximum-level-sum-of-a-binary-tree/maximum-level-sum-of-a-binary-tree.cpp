5f48c953a9af1346e9d9c2eda72edac11f0b6737
Shubham <58747139+shubham242@users.noreply.github.com>
2021-07-26 13:45:18 +0530

    Time: 136 ms (99.46%), Space: 104.9 MB (82.90%) - LeetHub

--- maximum-level-sum-of-a-binary-tree/maximum-level-sum-of-a-binary-tree.cpp
+++ maximum-level-sum-of-a-binary-tree/maximum-level-sum-of-a-binary-tree.cpp
@@ -9,37 +9,31 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+static const auto fast = []()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    return 0;
+}();
 class Solution {
 public:
     int result[10000]={0};
-    void maxSum(TreeNode* root, int level)
+    int maxSum(TreeNode* root, int level)
     {
         if(root==NULL)
-            return;
-        result[level]+=root->val;
-        if(root->left!=NULL)
-        maxSum(root->left,level+1);
-        if(root->right!=NULL)
-        maxSum(root->right,level+1);
-
-    }
-    int maxDepth(TreeNode* node)
-    {
-        if (node == NULL)
             return 0;
+        result[level]+=root->val;
+        
+        int l = maxSum(root->left,level+1);
+        int r= maxSum(root->right,level+1);
+        if(l>r)
+            return l+1;
         else
-        {
-            int lDepth = maxDepth(node->left);
-            int rDepth = maxDepth(node->right);
+            return r+1;
 
-            if (lDepth > rDepth)
-                return(lDepth + 1);
-            else return(rDepth + 1);
-        }
     }
     int maxLevelSum(TreeNode* root) {
-        maxSum(root,1);
-        int height = maxDepth(root);
+        int height = maxSum(root,1);
         for(int i=1;i<=height;i++)
             if(result[i]==*max_element(result+1, result + height+1))
                 return i;
