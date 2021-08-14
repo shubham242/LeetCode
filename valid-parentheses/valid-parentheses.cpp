9d2cd27e5af27444e419653f98faa2d94be620e4
Shubham <58747139+shubham242@users.noreply.github.com>
2021-08-14 22:29:29 +0530

    Time: 4 ms (8.42%), Space: 6.4 MB (47.78%) - LeetHub

---
+++ valid-parentheses/valid-parentheses.cpp
@@ -0,0 +1,30 @@
+class Solution
+{
+public:
+    bool isValid(string a)
+    {
+
+        stack<char> s;
+        for (int i = 0; i < a.size(); i++)
+        {
+            if (!s.empty())
+            {
+                if (s.top() == '(' && a[i] == ')')
+                    s.pop();
+                else if (s.top() == '[' && a[i] == ']')
+                    s.pop();
+                else if (s.top() == '{' && a[i] == '}')
+                    s.pop();
+                else
+                    s.push(a[i]);
+            }
+            else
+                s.push(a[i]);
+        }
+
+        if (s.empty())
+            return true;
+        else
+            return false;
+    }
+};
