e47b2960b5d2cb416d239873c6b7889034dafb9d
Shubham <58747139+shubham242@users.noreply.github.com>
2021-08-18 00:35:26 +0530

    Time: 8 ms (88.34%), Space: 21.9 MB (8.02%) - LeetHub

--- balanced-binary-tree/balanced-binary-tree.cpp
+++ balanced-binary-tree/balanced-binary-tree.cpp
@@ -9,18 +9,37 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+static const auto fast = []()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    return 0;
+}();
 class Solution {
 public:
-    bool ans=true;
-    int traverse(TreeNode* root){
-        if(!root)
-            return 0;
-        return max(traverse(root->left),traverse(root->right))+1;
+    bool isBal(TreeNode* root, int* height)
+{
+    int lh = 0, rh = 0;
+ 
+    int l = 0, r = 0;
+ 
+    if (root == NULL) {
+        *height = 0;
+        return 1;
     }
+ 
+    l = isBal(root->left, &lh);
+    r = isBal(root->right, &rh);
+ 
+    *height = (lh > rh ? lh : rh) + 1;
+ 
+    if (abs(lh - rh) >= 2)
+        return 0;
+    else
+        return l && r;
+}
     bool isBalanced(TreeNode* root) {
-        if(!root)
-            return true;
-        
-        return abs(traverse(root->left)-traverse(root->right))<=1 && isBalanced(root->left) && isBalanced(root->right);
+        int h=0;
+        return isBal(root,&h);
     }
 };
