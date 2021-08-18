e7d9f9f255879feec9615c319956ae1921e7fb4b
Shubham <58747139+shubham242@users.noreply.github.com>
2021-08-19 00:02:48 +0530

    Time: 12 ms (97.18%), Space: 16.8 MB (84.89%) - LeetHub

--- single-number/single-number.cpp
+++ single-number/single-number.cpp
@@ -3,14 +3,9 @@ class Solution
 public:
     int singleNumber(vector<int> &n)
     {
-        map<int, int> mp;
-
-        for (int i = 0; i < n.size(); i++)
-            if (mp[n[i]])
-                mp.erase(n[i]);
-            else
-                mp[n[i]]++;
-
-        return mp.begin()->first;
+        int x=0;
+        for(int i=0;i<n.size();i++)
+            x^=n[i];
+        return x;
     }
 };
