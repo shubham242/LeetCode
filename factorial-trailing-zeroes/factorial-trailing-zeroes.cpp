93f2dc4ba842bdd47b571634e27a88c90a853168
Shubham <58747139+shubham242@users.noreply.github.com>
2021-08-20 21:41:53 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (25.09%) - LeetHub

--- factorial-trailing-zeroes/factorial-trailing-zeroes.cpp
+++ factorial-trailing-zeroes/factorial-trailing-zeroes.cpp
@@ -4,13 +4,8 @@ public:
         long int r=5,ans=0;
         while(r<=n)
         {
-            int y=r;
-            while(y%5==0)
-            {
-                y=y/5;
-                ans++;
-            }
-            r+=5;
+            ans+=n/r;
+            r*=5;
         }
         return ans;
     }
