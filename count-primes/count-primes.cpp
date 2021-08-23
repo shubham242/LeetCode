b0d2b5a5f47ab7fd7ec0213629a748a4a07cee42
Shubham <58747139+shubham242@users.noreply.github.com>
2021-08-23 13:11:48 +0530

    Time: 48 ms (68.89%), Space: 11.4 MB (42.37%) - LeetHub

--- count-primes/count-primes.cpp
+++ count-primes/count-primes.cpp
@@ -1,23 +1,27 @@
-class Solution {
+
+class Solution
+{
 public:
-    int countPrimes(int n) {
-        if(!n)
+    int countPrimes(int n)
+    {
+        if (n == 0 || n == 1)
             return 0;
         bool prime[n];
+        int res = n - 2;
         memset(prime, true, sizeof(prime));
 
-        for (int p = 2; p * p <= n-1; p++)
+        for (int p = 2; p * p <= n - 1; p++)
         {
             if (prime[p] == true)
             {
-                for (int i = p * p; i <= n-1; i += p)
+                for (int i = p * p; i <= n - 1; i += p)
+                {
+                    if (prime[i])
+                        res--;
                     prime[i] = false;
+                }
             }
         }
-        int res=0;
-        for (int p = 2; p <= n-1; p++)
-            if (prime[p])
-                res++;
         return res;
     }
 };
