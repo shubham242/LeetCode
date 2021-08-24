ace52be2d295a977acba46d08368570bd19cdd9f
Shubham <58747139+shubham242@users.noreply.github.com>
2021-08-24 23:11:58 +0530

    Time: 8 ms (85.30%), Space: 7.4 MB (42.31%) - LeetHub

--- valid-anagram/valid-anagram.cpp
+++ valid-anagram/valid-anagram.cpp
@@ -4,14 +4,16 @@ public:
         if(s.size()!=t.size())
             return false;
         vector<int> a(26,0);
-        vector<int> b(26,0);
         
         for(int i=0;i<s.size();i++)
         {
             a[s[i]-97]++;
-            b[t[i]-97]++;
+            a[t[i]-97]--;
         }
         
-        return a==b;
+        for(int i=0;i<26;i++)
+            if(a[i]!=0)
+                return false;
+        return true;
     }
 };
