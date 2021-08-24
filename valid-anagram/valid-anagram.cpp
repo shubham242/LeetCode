0ece260dc5137c20c0f32a5bc7ee132316497178
Shubham <58747139+shubham242@users.noreply.github.com>
2021-08-24 23:14:24 +0530

    Time: 8 ms (85.30%), Space: 7.3 MB (75.94%) - LeetHub

--- valid-anagram/valid-anagram.cpp
+++ valid-anagram/valid-anagram.cpp
@@ -3,7 +3,7 @@ public:
     bool isAnagram(string s, string t) {
         if(s.size()!=t.size())
             return false;
-        vector<int> a(26,0);
+        int a[26]={0};
         
         for(int i=0;i<s.size();i++)
         {
