3227995c1a2b3a2588e4b25042ef30c002b2b421
Shubham <58747139+shubham242@users.noreply.github.com>
2021-09-01 15:31:00 +0530

    Time: 112 ms (99.36%), Space: 63.7 MB (99.68%) - LeetHub

--- contains-duplicate-ii/contains-duplicate-ii.cpp
+++ contains-duplicate-ii/contains-duplicate-ii.cpp
@@ -1,18 +1,24 @@
+static const auto fast = []()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    return 0;
+}();
 class Solution {
 public:
     bool containsNearbyDuplicate(vector<int> n, int k)
     {
-        unordered_map<int, int> mp;
-        int s=n.size();
-        for (int i = 0; i < s; i++)
+        int l = n.size();
+        pair<int, int> arr[l];
+        for (int i = 0; i < l; i++)
         {
-            if (mp[n[i]] > 0)
-                if (i + 1 - mp[n[i]] <= k)
-                    return true;
-                else
-                    mp[n[i]] = i + 1;
-            else
-                mp[n[i]] = i + 1;
+            arr[i] = make_pair(n[i], i);
+        }
+        sort(arr, arr + l);
+        for (int i = 0; i < l-1; i++)
+        {
+            if (arr[i].first == arr[i + 1].first && abs(arr[i].second - arr[i + 1].second) <= k)
+                return true;
         }
         return false;
     }
