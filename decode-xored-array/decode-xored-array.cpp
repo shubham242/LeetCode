3dd2a551d4d6ffab0c0398073a1c61afce23b354
Shubham <58747139+shubham242@users.noreply.github.com>
2021-09-03 22:31:30 +0530

    Time: 20 ms (96.81%), Space: 24.8 MB (85.98%) - LeetHub

--- decode-xored-array/decode-xored-array.cpp
+++ decode-xored-array/decode-xored-array.cpp
@@ -7,15 +7,9 @@ static const auto fast = []()
 class Solution {
 public:
     vector<int> decode(vector<int>& e, int first) {
-        vector<int> res;
-        res.push_back(first);
-        int l=e.size();
-        int curr=first;
-        for(int i=0;i<l;i++)
-        {
-            res.push_back(curr^e[i]);
-                curr=curr^e[i];
-        }
-        return res;
+        e.insert(e.begin(),first);
+        for(int i=1;i<e.size();i++)
+            e[i]=e[i]^e[i-1];
+        return e;
     }
 };
