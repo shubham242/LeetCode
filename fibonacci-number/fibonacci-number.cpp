2984af0eefad9510378d32b7fadb5522cac01e4b
Shubham <58747139+shubham242@users.noreply.github.com>
2021-09-10 03:24:20 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (18.34%) - LeetHub

--- fibonacci-number/fibonacci-number.cpp
+++ fibonacci-number/fibonacci-number.cpp
@@ -6,12 +6,8 @@ static const auto fast = []()
 }();
 class Solution {
 public:
-    int fib(int n) {
-        if(n==0)
-            return 0;
-        else if(n==1)
-            return 1;
-        else
-            return fib(n-1)+fib(n-2);
+    int fib(int N) {
+        double phi = (sqrt(5) + 1) / 2;     
+        return round(pow(phi, N) / sqrt(5));
     }
 };
