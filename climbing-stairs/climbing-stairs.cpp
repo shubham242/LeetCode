bbaab812c27aaedb31e37b37a887c2cecd6657fe
Shubham <58747139+shubham242@users.noreply.github.com>
2021-09-11 01:59:33 +0530

    Time: 4 ms (6.89%), Space: 6.1 MB (28.38%) - LeetHub

--- climbing-stairs/climbing-stairs.cpp
+++ climbing-stairs/climbing-stairs.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
-    int climbStairs(int n) {
-        double phi = (sqrt(5) + 1) / 2;     
-        return round(pow(phi, n+1) / sqrt(5));
+    int climbStairs(int n) { 
+        return round(pow((sqrt(5) + 1) / 2, n+1) / sqrt(5));
     }
 };
