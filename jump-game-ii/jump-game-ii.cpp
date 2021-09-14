ff48e38f700123110cd36a168312c77eaba70819
Shubham <58747139+shubham242@users.noreply.github.com>
2021-09-14 16:38:21 +0530

    Time: 119 ms (42.83%), Space: 16.4 MB (71.29%) - LeetHub

---
+++ jump-game-ii/jump-game-ii.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int dp(vector<int> &n, int l,int steps)
+    {
+        if (l == 0)
+            return steps;
+        for (int i = 0; i < l; i++)
+        {
+            if (i + n[i] >= l)
+            {
+                return dp(n,i,steps+1);
+                break;
+            }
+        }
+        return 0;
+    }
+    int jump(vector<int>& nums) {
+        
+        return dp(nums, nums.size() - 1,0);
+    }
+};
