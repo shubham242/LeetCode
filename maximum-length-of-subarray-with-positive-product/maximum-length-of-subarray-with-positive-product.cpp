c1a34ca3846826b3e33839f5f1a85bcef7ab30a2
Shubham <58747139+shubham242@users.noreply.github.com>
2021-09-15 03:07:13 +0530

    Time: 173 ms (18.65%), Space: 60.4 MB (10.97%) - LeetHub

--- maximum-length-of-subarray-with-positive-product/maximum-length-of-subarray-with-positive-product.cpp
+++ maximum-length-of-subarray-with-positive-product/maximum-length-of-subarray-with-positive-product.cpp
@@ -1,41 +1,23 @@
 class Solution {
 public:
-    int getMaxLen(vector<int> &nums)
-    {
-        int res = 0;
-        int l = nums.size();
-        int negCountr = 0, currMaxr = 0;
-        int negCountl = 0, currMaxl = 0;
-        for (int i = 0; i < l; i++)
+     int getMaxLen(vector<int> nums) 
+    { 
+        int framestart = -1, firstneg = -2, len = 0, neg = 0;
+        for (int i = 0; i < nums.size(); i++) 
         {
             if (nums[i] == 0)
+            {  
+                neg = 0;framestart = i;firstneg = -2;
+            } 
+            else 
             {
-                currMaxr = 0;
-                negCountr = 0;
-            }
-            else
-            {
-                if (nums[i] < 0)
-                    negCountr++;
-                currMaxr++;
-                if (negCountr % 2 == 0)
-                    res = max(res, currMaxr);
-            }
-            
-            if (nums[l-i-1] == 0)
-            {
-                currMaxl = 0;
-                negCountl = 0;
-            }
-            else
-            {
-                if (nums[l-i-1] < 0)
-                    negCountl++;
-                currMaxl++;
-                if (negCountl % 2 == 0)
-                    res = max(res, currMaxl);
+                if (nums[i] < 0) neg++;
+                if (neg == 1 && firstneg == -2) firstneg = i;
+                if (neg % 2 == 0) len = max(len, i - framestart);
+                else len = max(len, i - firstneg);
             }
         }
-        return res;
+        return len;
+
     }
 };
