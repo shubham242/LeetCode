def80dd06cfb91d158abceb5b8f84fcaa1fce304
Shubham <58747139+shubham242@users.noreply.github.com>
2021-10-12 03:56:05 +0530

    Time: 4 ms (71.53%), Space: 11.2 MB (81.47%) - LeetHub

--- best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -4,17 +4,15 @@ public:
         int n=prices.size();
         int b=INT_MIN;
         int s=0;
-        int c=0;
+        int nb;
+        int ns=0;
         
         for(auto p:prices)
-        {
-            int nb=b;
-            int ns=s;
-            int nc=c;
-            
-            b=max(nc-p,nb);
+        {   
+            nb=b;
+            b=max(ns-p,nb);
+            ns=s;
             s=max(nb+p,ns);
-            c=max(nc,ns);
         }
         
         return s;
