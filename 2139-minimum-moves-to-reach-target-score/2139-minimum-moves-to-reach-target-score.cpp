07d5119d63ddcd72ec59afcba328019bc1c19463
Shubham <58747139+shubham242@users.noreply.github.com>
2022-05-30 18:08:47 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (74.66%) - LeetHub

---
+++ 2139-minimum-moves-to-reach-target-score/2139-minimum-moves-to-reach-target-score.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int minMoves(int target, int maxDoubles) {
+        int res=0;
+        
+        while(target>1){
+            if(maxDoubles==0)
+                return res+target-1;
+            if(target%2==0&&maxDoubles){
+                target/=2;
+                maxDoubles--;
+            }
+            else{
+                target--;
+            }
+            res++;
+        }
+        return res;
+    }
+};
