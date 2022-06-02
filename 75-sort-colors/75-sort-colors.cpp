67d92e8883e03835a26a8e5d5678eaf8321e29b9
Shubham <58747139+shubham242@users.noreply.github.com>
2022-06-02 14:08:44 +0530

    Time: 0 ms (100.00%), Space: 8.4 MB (24.83%) - LeetHub

--- 75-sort-colors/75-sort-colors.cpp
+++ 75-sort-colors/75-sort-colors.cpp
@@ -3,37 +3,23 @@ class Solution
 public:
     void sortColors(vector<int> &nums)
     {
-        int l = nums.size();
-        int j = l - 1;
-        while (j >= 0 && nums[j] == 2)
-            j--;
+        int lo = 0;
+        int hi = nums.size() - 1;
+        int mid = 0;
 
-        for (int i = 0; i < j; i++)
+        while (mid <= hi)
         {
-            if (nums[i] == 2)
+            switch (nums[mid])
             {
-                nums[i] = nums[i] + nums[j];
-                nums[j] = nums[i] - nums[j];
-                nums[i] = nums[i] - nums[j];
-                j--;
-                while (j >= 0 && nums[j] == 2)
-                    j--;
-            }
-        }
-
-        while (j >= 0 && nums[j] == 1)
-            j--;
-
-        for (int i = 0; i < j; i++)
-        {
-            if (nums[i] == 1)
-            {
-                nums[i] = nums[i] + nums[j];
-                nums[j] = nums[i] - nums[j];
-                nums[i] = nums[i] - nums[j];
-                j--;
-                while (j >= 0 && nums[j] == 1)
-                    j--;
+            case 0:
+                swap(nums[lo++], nums[mid++]);
+                break;
+            case 1:
+                mid++;
+                break;
+            case 2:
+                swap(nums[hi--], nums[mid]);
+                break;
             }
         }
     }
