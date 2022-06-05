3bcdc578cef340e8d35d91f86b9a00e6e80f81a4
Shubham <58747139+shubham242@users.noreply.github.com>
2022-06-05 17:09:04 +0530

    Time: 11 ms (39.00%), Space: 6 MB (79.40%) - LeetHub

---
+++ 52-n-queens-ii/52-n-queens-ii.cpp
@@ -0,0 +1,47 @@
+class Solution
+{
+public:
+    bool isSafe(vector<string> &a, int i, int j, int n)
+    {
+        bool f = true;
+        for (int k = 1; i - k >= 0 || j + k < n || j - k >= 0; k++)
+            if ((i - k >= 0 && j - k >= 0 && a[i - k][j - k] == 'Q') || (i - k >= 0 && a[i - k][j] == 'Q') || (i - k >= 0 && j + k < n && a[i - k][j + k] == 'Q'))
+            {
+                f = false;
+                break;
+            }
+
+        return f;
+    }
+    void recur(vector<string> &a, int &ans, int n, int pos)
+    {
+        if (pos == n)
+        {
+            ans++;
+            return;
+        }
+        for (int i = 0; i < n; i++)
+        {
+            if (isSafe(a, pos, i, n))
+            {
+                a[pos][i] = 'Q';
+                recur(a, ans, n, pos + 1);
+                a[pos][i] = '.';
+            }
+        }
+    }
+    int totalNQueens(int n)
+    {
+
+        int ans = 0;
+        vector<string> a;
+        for (int i = 0; i < n; i++)
+        {
+            string s(n, '.');
+            a.push_back(s);
+        }
+
+        recur(a, ans, n, 0);
+        return ans;
+    }
+};
