ff02274c3751fd630934db22509e7d8a1bc954b8
Shubham <58747139+shubham242@users.noreply.github.com>
2022-06-06 16:00:16 +0530

    Time: 8 ms (19.63%), Space: 6.3 MB (85.17%) - LeetHub

---
+++ 206-reverse-linked-list/206-reverse-linked-list.c
@@ -0,0 +1,21 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+
+
+struct ListNode* reverseList(struct ListNode* head){
+
+        struct ListNode *prev = NULL;
+        while (head != NULL)
+        {
+            struct ListNode *next = head->next;
+            head->next = prev;
+            prev = head;
+            head = next;
+        }
+        return prev;
+}
