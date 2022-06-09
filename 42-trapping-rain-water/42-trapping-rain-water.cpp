32c8337cef76b1447314920bb23c726033396555
Shubham <58747139+shubham242@users.noreply.github.com>
2022-06-09 22:55:16 +0530

    Time: 743 ms (5.19%), Space: 19.8 MB (26.78%) - LeetHub

---
+++ 42-trapping-rain-water/42-trapping-rain-water.cpp
@@ -0,0 +1,28 @@
+class Solution
+{
+public:
+    int trap(vector<int> &h)
+    {
+        int l = h.size();
+        unordered_map<int, int> mp;
+        int sum = 0;
+
+        int maxH = 0;
+
+        for (int i = 0; i < l; i++)
+        {
+            maxH = 0;
+            if (h[i] > 0)
+                for (int j = i - 1; j >= 0; j--)
+                    if (h[j] > maxH)
+                    {
+                        sum += (i - j - 1) * (min(h[j], h[i]) - maxH);
+                        if (h[j] >= h[i])
+                            break;
+                        maxH = h[j];
+                    }
+        }
+
+        return sum;
+    }
+};
