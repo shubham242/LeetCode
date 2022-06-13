5588c0a26454b27adf4d2fd424d8c00afa60f230
Shubham <58747139+shubham242@users.noreply.github.com>
2022-06-13 15:34:38 +0530

    Time: 3 ms (97.83%), Space: 8.8 MB (55.37%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -5,13 +5,14 @@ public:
     {
         int n = triangle.size();
         vector<int> dp(n);
+        vector<int> temp = dp;
         for (int i = 0; i < n; i++)
             dp[i] = INT_MAX;
         dp[0] = triangle[0][0];
 
         for (int i = 1; i < n; i++)
         {
-            vector<int> temp = dp;
+            temp = dp;
             for (int j = 0; j <= i; j++)
                 temp[j] = triangle[i][j] + (j > 0 ? min(dp[j], dp[j - 1]) : dp[j]);
             dp = temp;
