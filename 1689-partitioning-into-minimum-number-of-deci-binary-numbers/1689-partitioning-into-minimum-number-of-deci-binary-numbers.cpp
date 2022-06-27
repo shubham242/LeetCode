830a9635ac414a98cadac39730f312e0b23ffb44
Shubham <58747139+shubham242@users.noreply.github.com>
2022-06-27 17:12:04 +0530

    Time: 41 ms (58.09%), Space: 13.5 MB (65.18%) - LeetHub

--- 1689-partitioning-into-minimum-number-of-deci-binary-numbers/1689-partitioning-into-minimum-number-of-deci-binary-numbers.cpp
+++ 1689-partitioning-into-minimum-number-of-deci-binary-numbers/1689-partitioning-into-minimum-number-of-deci-binary-numbers.cpp
@@ -3,13 +3,6 @@ class Solution
 public:
     int minPartitions(string n)
     {
-        int ans = 0;
-
-        for (auto i : n)
-            if (i - '0' == 9)
-                return 9;
-            else
-                ans = max(i - '0', ans);
-        return ans;
+        return *max_element(n.begin(),n.end())-'0';
     }
 };
