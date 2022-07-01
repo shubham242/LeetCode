82e60f11911058e2fda6ec2a04e0f5728b4880ee
Shubham <58747139+shubham242@users.noreply.github.com>
2022-07-01 23:32:17 +0530

    Time: 3 ms (97.11%), Space: 12.6 MB (96.46%) - LeetHub

--- 114-flatten-binary-tree-to-linked-list/114-flatten-binary-tree-to-linked-list.cpp
+++ 114-flatten-binary-tree-to-linked-list/114-flatten-binary-tree-to-linked-list.cpp
@@ -1,26 +1,15 @@
 class Solution
 {
 public:
-    TreeNode *righMost(TreeNode *root)
-    {
-        TreeNode *curr = root;
-        while (curr->right != NULL)
-            curr = curr->right;
-        return curr;
-    }
+    TreeNode *prev = NULL;
     void flatten(TreeNode *root)
     {
         if (root == NULL)
             return;
-        flatten(root->left);
         flatten(root->right);
-        if (root->left != NULL)
-        {
-            TreeNode *r = root->right;
-            TreeNode *rMost = righMost(root->left);
-            root->right = root->left;
-            root->left = NULL;
-            rMost->right = r;
-        }
+        flatten(root->left);
+        root->right = prev;
+        root->left = NULL;
+        prev = root;
     }
 };
