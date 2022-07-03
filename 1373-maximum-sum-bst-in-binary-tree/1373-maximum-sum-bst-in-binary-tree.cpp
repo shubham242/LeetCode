17ad017ed47b457a07f403213aa6b59907cff655
Shubham <58747139+shubham242@users.noreply.github.com>
2022-07-04 02:53:19 +0530

    Time: 203 ms (79.10%), Space: 104 MB (68.45%) - LeetHub

--- 1373-maximum-sum-bst-in-binary-tree/1373-maximum-sum-bst-in-binary-tree.cpp
+++ 1373-maximum-sum-bst-in-binary-tree/1373-maximum-sum-bst-in-binary-tree.cpp
@@ -3,25 +3,24 @@ class Solution
 public:
     struct container
     {
-        bool isValid;
         int sum;
         int largest;
         int smallest;
-        container(bool v, int s, int l, int sm) : isValid(v), sum(s), largest(l), smallest(sm){};
+        container(int s, int l, int sm) : sum(s), largest(l), smallest(sm){};
     };
     container isBST(TreeNode *root, int &ans)
     {
         if (root == NULL)
-            return container(true, 0, INT_MIN, INT_MAX);
+            return container(0, INT_MIN, INT_MAX);
         container l = isBST(root->left, ans);
         container r = isBST(root->right, ans);
 
-        if (l.isValid && r.isValid && root->val > l.largest && root->val < r.smallest)
+        if (root->val > l.largest && root->val < r.smallest)
         {
             ans = max(ans, root->val + l.sum + r.sum);
-            return container(true, root->val + l.sum + r.sum, max(root->val, r.largest), min(root->val, l.smallest));
+            return container(root->val + l.sum + r.sum, max(root->val, r.largest), min(root->val, l.smallest));
         }
-        return container(false, 0, INT_MIN, INT_MAX);
+        return container(0, INT_MAX, INT_MIN);
     }
     int maxSumBST(TreeNode *root)
     {
