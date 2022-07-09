f82b9384483e09df449070691f1f1a7f70500f04
Shubham <58747139+shubham242@users.noreply.github.com>
2022-07-09 14:59:47 +0530

    Time: 47 ms (92.12%), Space: 34 MB (54.33%) - LeetHub

--- 1306-jump-game-iii/1306-jump-game-iii.cpp
+++ 1306-jump-game-iii/1306-jump-game-iii.cpp
@@ -1,17 +1,17 @@
 class Solution
 {
 public:
-    unordered_map<int, int> mp;
     bool canReach(vector<int> &arr, int start)
     {
         if (arr[start] == 0)
             return true;
-        mp[start] = 1;
+        int x = arr[start];
+        arr[start] = -1;
 
-        if (start - arr[start] >= 0 && mp[start - arr[start]] == 0 && canReach(arr, start - arr[start]))
+        if (start - x >= 0 && arr[start - x] != -1 && canReach(arr, start - x))
             return true;
 
-        if (start + arr[start] < arr.size() && mp[start + arr[start]] == 0 && canReach(arr, start + arr[start]))
+        if (start + x < arr.size() && arr[start + x] != -1 && canReach(arr, start + x))
             return true;
 
         return false;
