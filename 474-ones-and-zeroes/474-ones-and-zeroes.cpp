83bd70c3ec734f51e304aa99799a268347dedb2f
Shubham <58747139+shubham242@users.noreply.github.com>
2022-07-14 03:26:15 +0530

    Time: 134 ms (92.75%), Space: 8.7 MB (98.58%) - LeetHub

--- 474-ones-and-zeroes/474-ones-and-zeroes.cpp
+++ 474-ones-and-zeroes/474-ones-and-zeroes.cpp
@@ -11,20 +11,22 @@ public:
     }
     int findMaxForm(vector<string> &strs, int m, int n)
     {
-        int dp[(int)strs.size() + 1][m + 1][n + 1];
+        int dp[m + 1][n + 1];
         memset(dp, 0, sizeof(dp));
 
         for (int i = 1; i <= strs.size(); i++)
         {
+            int prev[m + 1][n + 1];
+            copy(&dp[0][0], &dp[0][0] + (m + 1) * (n + 1), &prev[0][0]);
             pair<int, int> str = parse(strs[i - 1]);
             for (int j = 0; j <= m; j++)
                 for (int k = 0; k <= n; k++)
                     if (str.first <= j && str.second <= k)
-                        dp[i][j][k] = max(dp[i - 1][j][k], 1 + dp[i - 1][j - str.first][k - str.second]);
+                        dp[j][k] = max(prev[j][k], 1 + prev[j - str.first][k - str.second]);
                     else
-                        dp[i][j][k] = dp[i - 1][j][k];
+                        dp[j][k] = prev[j][k];
         }
 
-        return dp[strs.size()][m][n];
+        return dp[m][n];
     }
 };
