f766c10f792c28ce56991647fede4eac3454615b
Shubham <58747139+shubham242@users.noreply.github.com>
2022-07-14 00:53:30 +0530

    Time: 16 ms (85.30%), Space: 12.8 MB (56.73%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -5,15 +5,23 @@ public:
     {
         int l1 = text1.size();
         int l2 = text2.size();
-        vector<vector<int>> dp(l1 + 1, vector<int>(l2 + 1, 0));
+        if (l2 > l1)
+        {
+            swap(text1, text2);
+            swap(l1, l2);
+        }
+        vector<int> dp(l2 + 1, 0);
 
         for (int i = 1; i <= l1; i++)
+        {
+            vector<int> prev = dp;
             for (int j = 1; j <= l2; j++)
                 if (text1[i - 1] == text2[j - 1])
-                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                    dp[j] = 1 + prev[j - 1];
                 else
-                    dp[i][j] = max(dp[i][j - 1], dp[i - 1][j]);
+                    dp[j] = max(dp[j - 1], prev[j]);
+        }
 
-        return dp[l1][l2];
+        return dp[l2];
     }
 };
