050b2e71dcf37f9776b488ca8f84ba5c55f6d0f4
Shubham <58747139+shubham242@users.noreply.github.com>
2022-07-15 03:12:49 +0530

    Time: 120 ms (94.39%), Space: 8.5 MB (97.20%) - LeetHub

--- 879-profitable-schemes/879-profitable-schemes.cpp
+++ 879-profitable-schemes/879-profitable-schemes.cpp
@@ -9,19 +9,22 @@ public:
         memset(dp, 0, sizeof(dp));
         dp[0][0] = 1;
 
-        for (int i = 1; i <= l; i++)
+        for (int i = l; i > 0; i--)
         {
             long prev[minProfit + 1][n + 1];
             copy(&dp[0][0], &dp[0][0] + (minProfit + 1) * (n + 1), &prev[0][0]);
 
-            for (int j = 0; j <= minProfit; j++)
-                for (int k = 0; k <= n; k++)
-                    dp[j][k] = prev[j][k];
+            // for (int j = 0; j <= minProfit; j++)
+            //     for (int k = 0; k <= n; k++)
+            //         dp[j][k] = prev[j][k];
 
             for (int j = 0; j <= minProfit; j++)
                 for (int k = 0; k <= n; k++)
+                {
+                    // dp[j][k] = prev[j][k];
                     if (k + group[i - 1] <= n)
                         dp[min(minProfit, j + profit[i - 1])][k + group[i - 1]] += prev[j][k] % M;
+                }
         }
         long ans = 0;
         for (int i = 0; i <= n; i++)
