240231e8ec753f064781d76622e477c4eac8dec9
Shubham <58747139+shubham242@users.noreply.github.com>
2022-07-17 02:24:07 +0530

    Time: 18 ms (61.80%), Space: 7.1 MB (89.53%) - LeetHub

--- 72-edit-distance/72-edit-distance.cpp
+++ 72-edit-distance/72-edit-distance.cpp
@@ -1,27 +1,25 @@
 class Solution
 {
 public:
-    int dp[501][501];
-    int solve(string w1, string w2, int i, int j)
-    {
-        if (i == w1.size())
-            return w2.size() - j;
-        if (j == w2.size())
-            return w1.size() - i;
-        if (w1[i] == w2[j])
-            return solve(w1, w2, i + 1, j + 1);
-
-        if (dp[i][j] != -1)
-            return dp[i][j];
-        return dp[i][j] = min({
-                   1 + solve(w1, w2, i + 1, j),
-                   1 + solve(w1, w2, i, j + 1),
-                   1 + solve(w1, w2, i + 1, j + 1),
-               });
-    }
     int minDistance(string word1, string word2)
     {
-        memset(dp, -1, sizeof(dp));
-        return solve(word1, word2, 0, 0);
+        int m = word1.size();
+        int n = word2.size();
+        int dp[m + 1][n + 1];
+        memset(dp, 0, sizeof(dp));
+
+        for (int i = 0; i <= n; i++)
+            dp[0][i] = i;
+        for (int i = 0; i <= m; i++)
+            dp[i][0] = i;
+
+        for (int i = 1; i <= m; i++)
+            for (int j = 1; j <= n; j++)
+                if (word1[i - 1] == word2[j - 1])
+                    dp[i][j] = dp[i - 1][j - 1];
+                else
+                    dp[i][j] = 1 + min({dp[i - 1][j], dp[i][j - 1], dp[i - 1][j - 1]});
+
+        return dp[m][n];
     }
 };
