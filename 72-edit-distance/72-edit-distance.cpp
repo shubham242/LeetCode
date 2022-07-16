2884ee41f46bcc2ac3b1d38d93cfd9c82e32f89e
Shubham <58747139+shubham242@users.noreply.github.com>
2022-07-17 02:41:34 +0530

    Time: 15 ms (71.69%), Space: 6.2 MB (99.12%) - LeetHub

--- 72-edit-distance/72-edit-distance.cpp
+++ 72-edit-distance/72-edit-distance.cpp
@@ -5,21 +5,25 @@ public:
     {
         int m = word1.size();
         int n = word2.size();
-        int dp[m + 1][n + 1];
+        int dp[n + 1], prev[n + 1];
         memset(dp, 0, sizeof(dp));
+        memset(prev, 0, sizeof(prev));
 
         for (int i = 0; i <= n; i++)
-            dp[0][i] = i;
-        for (int i = 0; i <= m; i++)
-            dp[i][0] = i;
+            dp[i] = i;
 
         for (int i = 1; i <= m; i++)
+        {
+            copy(&dp[0], &dp[0] + n + 1, &prev[0]);
+            dp[0] = i;
+
             for (int j = 1; j <= n; j++)
                 if (word1[i - 1] == word2[j - 1])
-                    dp[i][j] = dp[i - 1][j - 1];
+                    dp[j] = prev[j - 1];
                 else
-                    dp[i][j] = 1 + min({dp[i - 1][j], dp[i][j - 1], dp[i - 1][j - 1]});
+                    dp[j] = 1 + min({prev[j], dp[j - 1], prev[j - 1]});
+        }
 
-        return dp[m][n];
+        return dp[n];
     }
 };
