45df43b7091c542dc222057ca774a3ff4abcc0c7
Shubham <58747139+shubham242@users.noreply.github.com>
2022-07-25 10:50:05 +0530

    Time: 414 ms (44.61%), Space: 60.4 MB (83.68%) - LeetHub

--- 973-k-closest-points-to-origin/973-k-closest-points-to-origin.cpp
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.cpp
@@ -1,7 +1,7 @@
 class Solution
 {
 public:
-    static bool comp(vector<int> v1, vector<int> v2)
+    static bool comp(vector<int> &v1, vector<int> &v2)
     {
         return (v1[0] * v1[0]) + (v1[1] * v1[1]) < (v2[0] * v2[0]) + (v2[1] * v2[1]);
     }
