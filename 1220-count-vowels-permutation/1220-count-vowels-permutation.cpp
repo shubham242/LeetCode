b1d2f8d3ff7cda9b70d3c5bb2f7666ea08621433
Shubham <58747139+shubham242@users.noreply.github.com>
2022-08-07 17:21:03 +0530

    Time: 4 ms (91.28%), Space: 5.9 MB (95.33%) - LeetHub

--- 1220-count-vowels-permutation/1220-count-vowels-permutation.cpp
+++ 1220-count-vowels-permutation/1220-count-vowels-permutation.cpp
@@ -4,20 +4,22 @@ public:
     int countVowelPermutation(int n)
     {
         int M = 1e9 + 7;
-        long dp[5][n];
+        long dp[5];
+        long prev[5];
         memset(dp, 0, sizeof(dp));
 
         for (int j = 0; j < 5; j++)
-            dp[j][0] = 1;
+            dp[j] = 1;
         for (int i = 1; i < n; i++)
         {
-            dp[0][i] = (dp[1][i - 1]) % M;
-            dp[1][i] = (dp[0][i - 1] + dp[2][i - 1]) % M;
-            dp[2][i] = (dp[0][i - 1] + dp[1][i - 1] + dp[3][i - 1] + dp[4][i - 1]) % M;
-            dp[3][i] = (dp[2][i - 1] + dp[4][i - 1]) % M;
-            dp[4][i] = (dp[0][i - 1]) % M;
+            copy(dp, dp + 5, prev);
+            dp[0] = (prev[1]) % M;
+            dp[1] = (prev[0] + prev[2]) % M;
+            dp[2] = (prev[0] + prev[1] + prev[3] + prev[4]) % M;
+            dp[3] = (prev[2] + prev[4]) % M;
+            dp[4] = (prev[0]) % M;
         }
 
-        return (dp[0][n - 1] + dp[1][n - 1] + dp[2][n - 1] + dp[3][n - 1] + dp[4][n - 1]) % M;
+        return (dp[0] + dp[1] + dp[2] + dp[3] + dp[4]) % M;
     }
 };
