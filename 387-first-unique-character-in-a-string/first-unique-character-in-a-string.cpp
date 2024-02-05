5689590db9d31193cb57da33ddfd1802b772c3c0
Shubham <58747139+shubham242@users.noreply.github.com>
2024-02-05 23:32:12 +0530

    Time: 16 ms (95.41%) | Memory: 11.8 MB (31.20%) - LeetSync

---
+++ 387-first-unique-character-in-a-string/first-unique-character-in-a-string.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        int hash[26]={0};
+
+        for(char ch : s)
+            hash[ch-'a']++;
+
+        for(int i=0;i<s.size();i++)
+            if(hash[s[i]-'a']==1)
+                return i;
+        
+        return -1;
+    }
+};
