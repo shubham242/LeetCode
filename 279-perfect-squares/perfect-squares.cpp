37df7fe72fcb1fdf8f1cb8ebbae91ebdfb3049ec
Shubham <58747139+shubham242@users.noreply.github.com>
2024-02-08 18:20:45 +0530

    Time: 33 ms (94.19%) | Memory: 7.3 MB (91.74%) - LeetSync

--- 279-perfect-squares/perfect-squares.cpp
+++ 279-perfect-squares/perfect-squares.cpp
@@ -1,21 +1,14 @@
 class Solution {
    public:
-    bool isSquare(int n) {
-        int sq = sqrt(n);
-        return sq * sq == n;
-    }
     int numSquares(int n) {
         int dp[n + 1];
 
-        for (int i = 1; i <= n; i++) {
+        for (int i = 0; i <= n; i++) {
             dp[i] = i;
-            if (isSquare(i)) {
-                dp[i] = 1;
-                continue;
-            }
             for (int j = 1; j * j <= i; j++)
                 dp[i] = min(dp[i], 1 + dp[i - j * j]);
         }
+
         return dp[n];
     }
 };
