fc0646485c6ccd9d4b02eadc5abaf30cbe69d2b3
Shubham <58747139+shubham242@users.noreply.github.com>
2024-02-13 13:33:32 +0530

    Time: 89 ms (14.96%) | Memory: 19.4 MB (21.96%) - LeetSync

--- 1559-cherry-pickup-ii/cherry-pickup-ii.cpp
+++ 1559-cherry-pickup-ii/cherry-pickup-ii.cpp
@@ -1,24 +1,25 @@
 class Solution {
    public:
-    int solve(vector<vector<int>>& grid, int rows, int cols, int x, int y1, int y2, vector<vector<vector<int>>>& dp) {
-        if (x >= rows || y1 >= cols || y2 >= cols || y1 < 0 || y2 < 0) return 0;
-        if (dp[x][y1][y2] != -1) return dp[x][y1][y2];
-
-        int ans = 0;
-        if (y1 == y2)
-            ans = grid[x][y1];
-        else
-            ans = grid[x][y1] + grid[x][y2];
-
-        int next = 0;
-        for (int i = -1; i <= 1; i++)
-            for (int j = -1; j <= 1; j++)
-                next = max(next, solve(grid, rows, cols, x + 1, y1 + i, y2 + j, dp));
-
-        return dp[x][y1][y2] = ans + next;
-    }
     int cherryPickup(vector<vector<int>>& grid) {
-        vector<vector<vector<int>>> dp(grid.size(), vector<vector<int>>(grid[0].size(), vector<int>(grid[0].size(), -1)));
-        return solve(grid, grid.size(), grid[0].size(), 0, 0, grid[0].size() - 1, dp);
+        int rows = grid.size(), cols = grid[0].size();
+        vector<vector<vector<int>>> dp(grid.size(), vector<vector<int>>(grid[0].size(), vector<int>(grid[0].size(), 0)));
+
+        for (int y1 = 0; y1 < cols; y1++)
+            for (int y2 = 0; y2 < cols; y2++)
+                dp[rows - 1][y1][y2] = grid[rows - 1][y1] + (y1 != y2 ? grid[rows - 1][y2] : 0);
+
+        for (int r = rows - 2; r >= 0; r--) {
+            for (int c1 = cols - 1; c1 >= 0; c1--) {
+                for (int c2 = cols - 1; c2 >= 0; c2--) {
+                    for (int i = -1; i <= 1; i++)
+                        for (int j = -1; j <= 1; j++)
+                            if (c1 + i >= 0 && c1 + i < cols && c2 + j >= 0 && c2 + j < cols)
+                                dp[r][c1][c2] = max(dp[r][c1][c2], dp[r + 1][c1 + i][c2 + j]);
+                    dp[r][c1][c2] += grid[r][c1] + (c1 != c2 ? grid[r][c2] : 0);
+                }
+            }
+        }
+        
+        return dp[0][0][cols - 1];
     }
 };
