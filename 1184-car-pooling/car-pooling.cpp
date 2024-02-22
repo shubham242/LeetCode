8853f84b360bf380245a38957163e19a218973e5
Shubham <58747139+shubham242@users.noreply.github.com>
2024-02-22 22:50:53 +0530

    Time: 6 ms (94.30%) | Memory: 13.3 MB (55.33%) - LeetSync

--- 1184-car-pooling/car-pooling.cpp
+++ 1184-car-pooling/car-pooling.cpp
@@ -4,11 +4,15 @@ public:
         int n = trips.size();
         vector<int> hash(1001,0);
         for(int i=0;i<n;i++){
-            for(int j=trips[i][1];j<trips[i][2];j++){
-                hash[j]+=trips[i][0];
-                if(hash[j]>capacity)
-                    return false;
-            }
+            hash[trips[i][1]]+=trips[i][0];
+            hash[trips[i][2]]-=trips[i][0];
+        }
+        int count=0;
+        for(int i=0;i<=1000;i++)
+        {
+            count+=hash[i];
+            if(count>capacity)
+                return false;
         }
 
         return true;
