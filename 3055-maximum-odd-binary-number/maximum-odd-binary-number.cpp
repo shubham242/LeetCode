71a340dde70a5f2195174e990cb52f7570784ac5
Shubham <58747139+shubham242@users.noreply.github.com>
2024-03-01 13:18:50 +0530

    Time: 4 ms (61.13%) | Memory: 9.2 MB (16.21%) - LeetSync

--- 3055-maximum-odd-binary-number/maximum-odd-binary-number.cpp
+++ 3055-maximum-odd-binary-number/maximum-odd-binary-number.cpp
@@ -2,19 +2,13 @@ class Solution {
    public:
     string maximumOddBinaryNumber(string s) {
         int one = 0, zero = 0;
-        string res = "";
         for (auto &c : s)
             if (c == '1')
                 one++;
             else
                 zero++;
 
-        for (int i = 1; i < one; i++)
-            res += "1";
-
-        for (int i = 1; i <= zero; i++)
-            res += "0";
-
-        return res + "1";
+        string ans = string(one - 1, '1') + string(zero, '0') + "1";
+        return ans;
     }
 };
