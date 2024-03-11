b3d998bd68246d685c47390f9c6009d2b8ee8761
Shubham <58747139+shubham242@users.noreply.github.com>
2024-03-11 17:32:34 +0530

    Time: 0 ms (100.00%) | Memory: 7.5 MB (66.00%) - LeetSync

--- 807-custom-sort-string/custom-sort-string.cpp
+++ 807-custom-sort-string/custom-sort-string.cpp
@@ -1,8 +1,15 @@
 class Solution {
    public:
     string customSortString(string order, string s) {
+        int size = s.size();
+        int order_size = order.size();
+        int hash[26] = {0};
+
+        for (int i = 0; i < order_size; i++)
+            hash[order[i] - 'a'] = i + 1;
+
         sort(s.begin(), s.end(), [&](char a, char b) {
-            return order.find(a) < order.find(b);
+            return hash[a - 'a'] < hash[b - 'a'];
         });
 
         return s;
