0cabc51d3983e48a2e6a8d85476f3fa02d143ebc
Shubham <58747139+shubham242@users.noreply.github.com>
2024-03-14 19:20:25 +0530

    Time: 54 ms (25.89%) | Memory: 40 MB (14.58%) - LeetSync

---
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
@@ -0,0 +1,13 @@
+class Solution {
+   public:
+    int numSubarraysWithSum(vector<int>& nums, int goal) {
+        unordered_map<int, int> count;
+        int sum = 0, res = 0;
+        for (int num : nums) {
+            count[sum]++;
+            sum += num;
+            res += count[sum - goal];
+        }
+        return res;
+    }
+};
