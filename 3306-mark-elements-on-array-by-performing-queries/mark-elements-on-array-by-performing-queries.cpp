97a83d9ee752076bd13a98f8b72723bc2ee39952
Shubham <58747139+shubham242@users.noreply.github.com>
2024-03-17 01:20:53 +0530

    Time: 256 ms (100.00%) | Memory: 129.9 MB (100.00%) - LeetSync

--- 3306-mark-elements-on-array-by-performing-queries/mark-elements-on-array-by-performing-queries.cpp
+++ 3306-mark-elements-on-array-by-performing-queries/mark-elements-on-array-by-performing-queries.cpp
@@ -8,8 +8,6 @@ class Solution {
 
         priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
 
-        vector<bool> marked(n, false);
-
         for (int i = 0; i < n; ++i) {
             pq.push({nums[i], i});
             sum += nums[i];
@@ -18,17 +16,15 @@ class Solution {
         for (int i = 0; i < m; ++i) {
             int index = queries[i][0];
             int k = queries[i][1];
+            sum -= nums[index];
+            nums[index] = 0;
 
-            if (!marked[index]) {
-                marked[index] = true;
-                sum -= nums[index];
-            }
             while (k > 0 && !pq.empty()) {
                 auto [value, idx] = pq.top();
                 pq.pop();
-                if (!marked[idx]) {
-                    marked[idx] = true;
-                    sum -= value;
+                if (nums[idx]) {
+                    sum -= nums[idx];
+                    nums[idx] = 0;
                     k--;
                 }
             }
