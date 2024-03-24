71051082ce1342df3124093c97203f05da09b667
Shubham <58747139+shubham242@users.noreply.github.com>
2024-03-24 15:02:33 +0530

    Time: 72 ms (82.20%) | Memory: 63.5 MB (77.16%) - LeetSync

---
+++ 287-find-the-duplicate-number/find-the-duplicate-number.cpp
@@ -0,0 +1,13 @@
+class Solution
+{
+public:
+    int findDuplicate(vector<int> &nums){
+        for (int num : nums) {
+            int idx = abs(num);
+            if (nums[idx] < 0) 
+                return idx;
+            nums[idx] = -nums[idx];
+        }
+        return 0;
+    }
+};
