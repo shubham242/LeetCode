9198a3e63e37b3ef0e674f36e2f17e9b3860c12f
Shubham <58747139+shubham242@users.noreply.github.com>
2024-04-03 16:15:28 +0530

    Time: 1018 ms (24.75%) | Memory: 11.7 MB (49.58%) - LeetSync

--- 79-word-search/word-search.cpp
+++ 79-word-search/word-search.cpp
@@ -1,24 +1,24 @@
 class Solution {
    public:
-    bool dfs(vector<vector<char>>& board, string word, int x, int y, int count, vector<vector<bool>>& vis) {
+    bool dfs(vector<vector<char>>& board, string word, int x, int y, int count) {
         if (count == word.size())
             return true;
-        if (x < 0 || x >= board.size() || y < 0 || y >= board[0].size() || vis[x][y] || board[x][y] != word[count])
+        if (x < 0 || x >= board.size() || y < 0 || y >= board[0].size() || board[x][y] != word[count])
             return false;
-        vis[x][y] = true;
-        bool found = dfs(board, word, x + 1, y, count + 1, vis) ||
-                     dfs(board, word, x - 1, y, count + 1, vis) ||
-                     dfs(board, word, x, y + 1, count + 1, vis) ||
-                     dfs(board, word, x, y - 1, count + 1, vis);
-        vis[x][y] = false;
+        char temp = board[x][y];
+        board[x][y] = '*';
+        bool found = dfs(board, word, x + 1, y, count + 1) ||
+                     dfs(board, word, x - 1, y, count + 1) ||
+                     dfs(board, word, x, y + 1, count + 1) ||
+                     dfs(board, word, x, y - 1, count + 1);
+        board[x][y] = temp;
         return found;
     }
     bool exist(vector<vector<char>>& board, string word) {
         int n = board.size(), m = board[0].size();
-        vector<vector<bool>> vis(n, vector<bool>(m, false));
         for (int i = 0; i < n; i++)
             for (int j = 0; j < m; j++)
-                if (dfs(board, word, i, j, 0, vis))
+                if (dfs(board, word, i, j, 0))
                     return true;
         return false;
     }
