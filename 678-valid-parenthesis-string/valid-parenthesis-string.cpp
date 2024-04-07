3374cd1ff96420095115c43e7d36be863efcd593
Shubham <58747139+shubham242@users.noreply.github.com>
2024-04-07 23:46:13 +0530

    Time: 3 ms (45.50%) | Memory: 9.3 MB (10.86%) - LeetSync

---
+++ 678-valid-parenthesis-string/valid-parenthesis-string.cpp
@@ -0,0 +1,25 @@
+class Solution {
+   public:
+    bool checkValidString(string s) {
+        vector<vector<int>> dp(s.size() + 1, vector<int>(s.size() + 1, 0));
+        dp[s.size()][0] = 1;
+
+        for (int ind = s.size() - 1; ind >= 0; ind--) {
+            for (int openingBracket = 0; openingBracket < s.size(); openingBracket++) {
+                bool ans = false;
+                if (s[ind] == '*') {
+                    ans |= dp[ind + 1][openingBracket + 1];
+                    if (openingBracket) ans |= dp[ind + 1][openingBracket - 1];
+                    ans |= dp[ind + 1][openingBracket];
+                } else if (s[ind] == '(')
+                    ans |= dp[ind + 1][openingBracket + 1];
+                else if (openingBracket)
+                    ans |= dp[ind + 1][openingBracket - 1];
+
+                dp[ind][openingBracket] = ans;
+            }
+        }
+
+        return dp[0][0];
+    }
+};
